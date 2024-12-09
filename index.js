b4461ae0116d65a6d1ff43acecfd9e2a1ab175ef
Ali-humza <lexoff2023@gmail.com>
2024-12-09 19:56:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("hello i am a conditional tutorial")
+
+let age = 1;
+let grace = 3 ;
+console.log(age% grace);
+console.log(age** grace);
+console.log(age/ grace);
+
+
+if (age > 18) {
+    console.log("u can drive");
+}
+else if(age==0){
+    console.log("are u kidding me?")
+}
+
+else if(age==1){
+    console.log("are u again kidding me?")
+}
+else {
+    console.log("u cannot drive");
+}
+
+a=9;
+b=2
+
+let c = a>b ? (a-b) : (b-a);
+
